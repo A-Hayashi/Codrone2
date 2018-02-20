3e6100758d63918d660a782a5394dba0f4f86e46
hayashi <a.hayashi0113@gmail.com>
2018-02-21 02:30:34 +0900

    5

--- Controller_Flight_3/Controller_Flight_3.h
+++ Controller_Flight_3/Controller_Flight_3.h
@@ -1,3 +1,7 @@
+#define EEP_START_ADRESS 20
+#define RANGE_CHECK(x,min,max) ((x= (x<min  ? min : x<max ? x : max)))
+#define LowB(x) (x & 0xFF)
+#define HighB(x) (x >> 8)
 
 typedef struct _EEPSTRUCT {
   int YawTrim;
@@ -18,38 +22,120 @@ typedef struct _EEPSTRUCT {
   double Roll_Td;
 } EEPSTRUCT;
 
-#define EEP_START_ADRESS 20
-#define RANGE_CHECK(x,min,max) ((x= (x<min  ? min : x<max ? x : max)))
-#define LowB(x) (x & 0xFF)
-#define HighB(x) (x >> 8)
+typedef struct _COMMAND_STRUCT {
+  byte Command1;
+  byte Command2;
+  byte Command3;
+  byte Command4;
+} COMMAND_STRUCT;
+
+typedef struct _OUTPUT_STRUCT {
+  int Yaw;
+  int Throttle;
+  int Pitch;
+  int Roll;
+} OUTPUT_STRUCT;
+
+typedef struct _TARGET_STRUCT {
+  int Yaw;
+  int Throttle;
+  int Pitch;
+  int Roll;
+} TARGET_STRUCT;
+
 
 enum tType
 {
-	tType_Attitude,
-	tType_GyroBias,
-	tType_TrimAll,
-	tType_IrMessage,
-	tType_ImuRawAndAngl,
-	tType_Pressure,
-	tType_Temperature,
+  tType_Attitude,
+  tType_GyroBias,
+  tType_TrimAll,
+  tType_IrMessage,
+  tType_ImuRawAndAngl,
+  tType_Pressure,
+  tType_Temperature,
   tType_AnalogStick
 };
 
 enum rType
 {
-	rType_Command,
-	rType_TrimAll,
-	rType_Gains
+  rType_Command,
+  rType_TrimAll,
+  rType_Target,
+  rType_Output,
+  rType_Gains
 };
 
 enum cmdType
 {
-	cmdType_Control,
-	cmdType_EEP_Write,
-	cmdType_EEP_Read,
-	cmdType_TrimTune,
+  cmdType_Control,
+  cmdType_EEP_Write,
+  cmdType_EEP_Read,
+  cmdType_TrimTune,
   cmdType_TrimSet,
-	cmdType_Stop,
-	cmdType_Hover,
-	cmdType_GainTune,
+  cmdType_Stop,
+  cmdType_Hover,
+  cmdType_GainTune,
 };
+
+class EepMgrClass {
+  public:
+    EEPSTRUCT Data;
+    void writeall();
+    void readall();
+};
+
+
+class AnalogStickClass {
+  public:
+    int Yaw, Throttle, Pitch, Roll;
+    void get_sens(void);
+};
+
+class SendPcClass {
+  public:
+    void send();
+  private:
+    void Send_Attitude();
+    void Send_AnalogStick();
+    void Send_GyroBias();
+    void Send_TrimAll();
+    void Send_IrMessage();
+    void Send_ImuRawAndAngl();
+    void Send_Pressure();
+    void Send_Temperature();
+    void Send_Processing(byte _cType, byte *_data, byte _length);
+};
+
+
+class IrSensClass {
+  public:
+    byte Data;
+    void get_sens();
+    void port_setting();
+};
+
+class ReceivePcClass {
+  public:
+    COMMAND_STRUCT Command;
+    OUTPUT_STRUCT Output;
+    TARGET_STRUCT Target;
+    void Receive();
+  private:
+    byte cmdBuff[MAX_PACKET_LENGTH];
+    byte dataBuff[MAX_PACKET_LENGTH];
+    byte cmdIndex;
+    byte checkHeader;
+    byte receiveDtype;
+    byte receiveLength;
+
+    void Receive_Command();
+    void Receive_TrimAll();
+    void Receive_Output();
+    void Receive_Target();
+    void Receive_Gains();
+    int UniteByte(byte upper, byte lower);
+};
+
+
+
+
--- Controller_Flight_3/Controller_Flight_3.ino
+++ Controller_Flight_3/Controller_Flight_3.ino
@@ -12,9 +12,10 @@
 #include "Controller_Flight_3.h"
 
 SoftwareSerial mySerial(27, 28); /* Rx,Tx */
-EEPSTRUCT EEP;
-int Yaw, Throttle, Pitch, Roll;
-byte IR_Sensor;
+AnalogStickClass AnalogStick;
+IrSensClass IrSens;
+EepMgrClass EepMgr;
+SendPcClass SendPc;
 
 void setup()
 {
@@ -22,42 +23,18 @@ void setup()
   CoDrone.AutoConnect(NearbyDrone);    // finds and connects to a drone that is nearby
   CoDrone.DroneModeChange(Flight);    // Changes the drone so that it is now in flight mode
   mySerial.begin(9600);
-  eep_read();
-  for (int i = 0; i < 8; i++) {
-    pinMode(11 + i, INPUT);
-  }
-}
-
-void get_analog_stick(void)
-{
-  Yaw = -1 * CoDrone.AnalogScaleChange(analogRead(A3)); // YAW (turn left or right), port A3, reversed
-  Throttle = CoDrone.AnalogScaleChange(analogRead(A4)); // THROTTLE (height), port A4, not reversed
-  Roll = -1 * CoDrone.AnalogScaleChange(analogRead(A5)); // ROLL (tilt left or right), port A5, reversed
-  Pitch = CoDrone.AnalogScaleChange(analogRead(A6)); // PITCH (tilt front or back), port A6, not reversed
-}
-
-
-void get_ir_sensor()
-{
-  byte ir[8];
-
-  for (int i = 0; i < 8; i++) {
-    ir[i] = digitalRead(11 + i);
-  }
-  IR_Sensor = 0;
-  for (int i = 0; i < 8; i++) {
-    IR_Sensor |= (ir[i] << (7 - i));
-  }
+  EepMgr.readall();
+  IrSens.port_setting();
 }
 
 void loop()
 {
   receive_pcdata();
   get_dronedata();
-  get_ir_sensor();
-  get_analog_stick();
+  IrSens.get_sens();
+  AnalogStick.get_sens();
   state_machine();
-  send_pcdata();
+  SendPc.send();
 }
 
 void get_dronedata()
@@ -81,159 +58,6 @@ void get_dronedata()
 void receive_pcdata()
 {
 }
-
-void send_pcdata()
-{
-  Metro interval50 = Metro(50);
-  Metro interval2500 = Metro(2500);
-
-  if (interval50.check()) {
-    Send_Attitude();
-    Send_ImuRawAndAngl();
-    Send_Pressure();
-    Send_IrMessage();
-    Send_AnalogStick();
-  }
-
-  if (interval2500.check()) {
-    Send_GyroBias();
-    Send_TrimAll();
-    Send_Temperature();
-  }
-}
-
-
-void Send_Attitude()
-{
-  byte data[12];
-  byte len = 7;
-  data[0] = LowB(CoDrone.droneAttitude[0]);   //Roll
-  data[1] = HighB(CoDrone.droneAttitude[0]);
-  data[2] = LowB(CoDrone.droneAttitude[1]);   //Pitch
-  data[3] = HighB(CoDrone.droneAttitude[1]);
-  data[4] = LowB(CoDrone.droneAttitude[2]);   //Yaw
-  data[5] = HighB(CoDrone.droneAttitude[2]);
-  data[6] = CoDrone.Alive.Attitude;
-  Send_Processing(tType_Attitude, data, len);
-}
-
-
-void Send_AnalogStick()
-{
-  byte data[12];
-  byte len = 8;
-  data[0] = LowB(Roll);
-  data[1] = HighB(Roll);
-  data[2] = LowB(Pitch);
-  data[3] = HighB(Pitch);
-  data[4] = LowB(Yaw);
-  data[5] = HighB(Yaw);
-  data[6] = LowB(Throttle);
-  data[7] = HighB(Throttle);
-  Send_Processing(tType_AnalogStick, data, len);
-}
-
-
-void Send_GyroBias()
-{
-  byte data[12];
-  byte len = 7;
-  data[0] = CoDrone.droneGyroBias[0]; //Roll
-  data[1] = CoDrone.droneGyroBias[1];
-  data[2] = CoDrone.droneGyroBias[2]; //Pitch
-  data[3] = CoDrone.droneGyroBias[3];
-  data[4] = CoDrone.droneGyroBias[4]; //Yaw
-  data[5] = CoDrone.droneGyroBias[5];
-  data[6] = CoDrone.Alive.GyroBias;
-  Send_Processing(tType_GyroBias, data, len);
-}
-
-
-void Send_TrimAll()
-{
-  byte data[12];
-  byte len = 11;
-  data[0] = CoDrone.droneTrimAll[0];  //Roll
-  data[1] = CoDrone.droneTrimAll[1];
-  data[2] = CoDrone.droneTrimAll[2];  //Pitch
-  data[3] = CoDrone.droneTrimAll[3];
-  data[4] = CoDrone.droneTrimAll[4];  //Yaw
-  data[5] = CoDrone.droneTrimAll[5];
-  data[6] = CoDrone.droneTrimAll[6];  //Throttle
-  data[7] = CoDrone.droneTrimAll[7];
-  data[8] = CoDrone.droneTrimAll[8];  //Wheel
-  data[9] = CoDrone.droneTrimAll[9];
-  data[10] = CoDrone.Alive.TrimAll;
-  Send_Processing(tType_TrimAll, data, len);
-}
-
-
-void Send_IrMessage()
-{
-  byte data[12];
-  byte len = 6;
-  data[0] = CoDrone.droneIrMassage[0];  //Direction
-  data[1] = CoDrone.droneIrMassage[1];
-  data[2] = CoDrone.droneIrMassage[2];
-  data[3] = CoDrone.droneIrMassage[3];
-  data[4] = CoDrone.droneIrMassage[4];
-  data[5] = CoDrone.Alive.IrMessage;
-  Send_Processing(tType_IrMessage, data, len);
-}
-
-
-void Send_ImuRawAndAngl()
-{
-  byte data[19];
-  byte len = 19;
-  data[0] = LowB(CoDrone.droneImuRawAndAngle[0]);   //AccX
-  data[1] = HighB(CoDrone.droneImuRawAndAngle[0]);
-  data[2] = LowB(CoDrone.droneImuRawAndAngle[1]);   //AccY
-  data[3] = HighB(CoDrone.droneImuRawAndAngle[1]);
-  data[4] = LowB(CoDrone.droneImuRawAndAngle[2]);   //AccZ
-  data[5] = HighB(CoDrone.droneImuRawAndAngle[2]);
-  data[6] = LowB(CoDrone.droneImuRawAndAngle[3]);   //GyroRoll
-  data[7] = HighB(CoDrone.droneImuRawAndAngle[3]);
-  data[8] = LowB(CoDrone.droneImuRawAndAngle[4]);   //GyroPitch
-  data[9] = HighB(CoDrone.droneImuRawAndAngle[4]);
-  data[10] = LowB(CoDrone.droneImuRawAndAngle[5]);  //GyroYaw
-  data[11] = HighB(CoDrone.droneImuRawAndAngle[5]);
-  data[12] = LowB(CoDrone.droneImuRawAndAngle[6]);  //AngleRoll
-  data[13] = HighB(CoDrone.droneImuRawAndAngle[6]);
-  data[14] = LowB(CoDrone.droneImuRawAndAngle[7]);  //AnglePitch
-  data[15] = HighB(CoDrone.droneImuRawAndAngle[7]);
-  data[16] = LowB(CoDrone.droneImuRawAndAngle[8]);  //AngleYaw
-  data[17] = HighB(CoDrone.droneImuRawAndAngle[8]);
-  data[18] = CoDrone.Alive.ImuRawAndAngle;
-
-  Send_Processing(tType_ImuRawAndAngl, data, len);
-}
-
-void Send_Pressure()
-{
-  byte data[17];
-  byte len = 17;
-
-  for (int i = 0; i < len-1; i++) {
-    data[i] = CoDrone.dronePressure[i];
-  }
-  data[16] = CoDrone.Alive.Pressure;
-  Send_Processing(tType_Pressure, data, len);
-}
-
-void Send_Temperature()
-{
-  byte data[9];
-  byte len = 9;
-
-  for (int i = 0; i < len-1; i++) {
-    data[i] = CoDrone.droneTemperature[i];
-  }
-  data[8] = CoDrone.Alive.Temperature;
-  Send_Processing(tType_Temperature, data, len);
-}
-
-
 
 byte Command;
 byte trans_state(byte state)
@@ -259,7 +83,7 @@ void state_machine()
 
   switch (state) {
     case cmdType_Control:
-//      state_Control(state_change);
+      //      state_Control(state_change);
       break;
     case cmdType_EEP_Write:
       state_EEP_Write(state_change);
@@ -283,7 +107,7 @@ void state_machine()
       state_GainTune(state_change);
       break;
     default:
-//      state_Control(state_change);
+      //      state_Control(state_change);
       break;
   }
   old_state = state;
@@ -291,17 +115,17 @@ void state_machine()
 
 void state_Control(boolean state_change)
 {
-  YAW       = Yaw;      // Set the A3 analog pin to control the Yaw
-  THROTTLE  = Throttle; // Set the A4 analog pin to control the Throttle
-  ROLL      = Roll;     // Set the A5 analog pin to control the Roll
-  PITCH     = Pitch;    // Set the A6 analog pin to control the Pitch
+  YAW       = AnalogStick.Yaw;      // Set the A3 analog pin to control the Yaw
+  THROTTLE  = AnalogStick.Throttle; // Set the A4 analog pin to control the Throttle
+  ROLL      = AnalogStick.Roll;     // Set the A5 analog pin to control the Roll
+  PITCH     = AnalogStick.Pitch;    // Set the A6 analog pin to control the Pitch
   CoDrone.Control(SEND_INTERVAL); // Send the new flight commands at the SEND_INTERVAL (50ms)
 }
 
 void state_EEP_Write(boolean state_change)
 {
   if (state_change) {
-    eep_write();
+    EepMgr.writeall();
     CoDrone.Buzz(2000, 4);
     CoDrone.Buzz(4000, 4);
   }
@@ -310,7 +134,7 @@ void state_EEP_Write(boolean state_change)
 void state_EEP_Read(boolean state_change)
 {
   if (state_change) {
-    eep_read();
+    EepMgr.readall();
     CoDrone.Buzz(2000, 4);
     CoDrone.Buzz(4000, 4);
   }
@@ -318,14 +142,14 @@ void state_EEP_Read(boolean state_change)
 
 void state_TrimTune(boolean state_change)
 {
-  EEP.YawTrim = 0;
+  EepMgr.Data.YawTrim = 0;
 }
 
 
 void state_TrimSet(boolean state_change)
 {
   if (state_change) {
-    CoDrone.Set_TrimAll(EEP.RollTrim, EEP.PitchTrim, EEP.YawTrim, EEP.ThrottleTrim, 0);
+    CoDrone.Set_TrimAll(EepMgr.Data.RollTrim, EepMgr.Data.PitchTrim, EepMgr.Data.YawTrim, EepMgr.Data.ThrottleTrim, 0);
     CoDrone.Buzz(2000, 4);
     CoDrone.Buzz(4000, 4);
   }
@@ -347,7 +171,178 @@ void state_GainTune(boolean state_change)
 
 }
 
-void Send_Processing(byte _cType, byte *_data, byte _length)
+
+
+void clearAndHome()
+{
+  mySerial.write(27); // ESC
+  mySerial.print("[2J"); // clear screen
+  mySerial.write(27); // ESC
+  mySerial.print("[H"); // cursor to home
+}
+
+
+void AnalogStickClass::get_sens(void)
+{
+  Yaw = -1 * CoDrone.AnalogScaleChange(analogRead(A3)); // YAW (turn left or right), port A3, reversed
+  Throttle = CoDrone.AnalogScaleChange(analogRead(A4)); // THROTTLE (height), port A4, not reversed
+  Roll = -1 * CoDrone.AnalogScaleChange(analogRead(A5)); // ROLL (tilt left or right), port A5, reversed
+  Pitch = CoDrone.AnalogScaleChange(analogRead(A6)); // PITCH (tilt front or back), port A6, not reversed
+}
+
+void EepMgrClass::writeall()
+{
+  byte* p = (byte*)&Data;
+  for (int j = 0; j < sizeof(EEPSTRUCT); j++) {
+    EEPROM.write(j + EEP_START_ADRESS, *p);
+    p++;
+  }
+}
+void EepMgrClass::readall()
+{
+  byte* p = (byte*)&Data;
+  for (int j = 0; j < sizeof(EEPSTRUCT); j++) {
+    *p = EEPROM.read(j + EEP_START_ADRESS);
+    p++;
+  }
+}
+
+void SendPcClass::send()
+{
+  Metro interval50 = Metro(50);
+  Metro interval2500 = Metro(2500);
+  if (interval50.check()) {
+    Send_Attitude();
+    Send_ImuRawAndAngl();
+    Send_Pressure();
+    Send_IrMessage();
+    Send_AnalogStick();
+  }
+  if (interval2500.check()) {
+    Send_GyroBias();
+    Send_TrimAll();
+    Send_Temperature();
+  }
+}
+
+void SendPcClass::Send_Attitude()
+{
+  byte data[12];
+  byte len = 7;
+  data[0] = LowB(CoDrone.droneAttitude[0]);   //Roll
+  data[1] = HighB(CoDrone.droneAttitude[0]);
+  data[2] = LowB(CoDrone.droneAttitude[1]);   //Pitch
+  data[3] = HighB(CoDrone.droneAttitude[1]);
+  data[4] = LowB(CoDrone.droneAttitude[2]);   //Yaw
+  data[5] = HighB(CoDrone.droneAttitude[2]);
+  data[6] = CoDrone.Alive.Attitude;
+  Send_Processing(tType_Attitude, data, len);
+}
+void SendPcClass::Send_AnalogStick()
+{
+  byte data[12];
+  byte len = 8;
+  data[0] = LowB(AnalogStick.Roll);
+  data[1] = HighB(AnalogStick.Roll);
+  data[2] = LowB(AnalogStick.Pitch);
+  data[3] = HighB(AnalogStick.Pitch);
+  data[4] = LowB(AnalogStick.Yaw);
+  data[5] = HighB(AnalogStick.Yaw);
+  data[6] = LowB(AnalogStick.Throttle);
+  data[7] = HighB(AnalogStick.Throttle);
+  Send_Processing(tType_AnalogStick, data, len);
+}
+void SendPcClass::Send_GyroBias()
+{
+  byte data[12];
+  byte len = 7;
+  data[0] = CoDrone.droneGyroBias[0]; //Roll
+  data[1] = CoDrone.droneGyroBias[1];
+  data[2] = CoDrone.droneGyroBias[2]; //Pitch
+  data[3] = CoDrone.droneGyroBias[3];
+  data[4] = CoDrone.droneGyroBias[4]; //Yaw
+  data[5] = CoDrone.droneGyroBias[5];
+  data[6] = CoDrone.Alive.GyroBias;
+  Send_Processing(tType_GyroBias, data, len);
+}
+void SendPcClass::Send_TrimAll()
+{
+  byte data[12];
+  byte len = 11;
+  data[0] = CoDrone.droneTrimAll[0];  //Roll
+  data[1] = CoDrone.droneTrimAll[1];
+  data[2] = CoDrone.droneTrimAll[2];  //Pitch
+  data[3] = CoDrone.droneTrimAll[3];
+  data[4] = CoDrone.droneTrimAll[4];  //Yaw
+  data[5] = CoDrone.droneTrimAll[5];
+  data[6] = CoDrone.droneTrimAll[6];  //Throttle
+  data[7] = CoDrone.droneTrimAll[7];
+  data[8] = CoDrone.droneTrimAll[8];  //Wheel
+  data[9] = CoDrone.droneTrimAll[9];
+  data[10] = CoDrone.Alive.TrimAll;
+  Send_Processing(tType_TrimAll, data, len);
+}
+void SendPcClass::Send_IrMessage()
+{
+  byte data[12];
+  byte len = 6;
+  data[0] = CoDrone.droneIrMassage[0];  //Direction
+  data[1] = CoDrone.droneIrMassage[1];
+  data[2] = CoDrone.droneIrMassage[2];
+  data[3] = CoDrone.droneIrMassage[3];
+  data[4] = CoDrone.droneIrMassage[4];
+  data[5] = CoDrone.Alive.IrMessage;
+  Send_Processing(tType_IrMessage, data, len);
+}
+void SendPcClass::Send_ImuRawAndAngl()
+{
+  byte data[19];
+  byte len = 19;
+  data[0] = LowB(CoDrone.droneImuRawAndAngle[0]);   //AccX
+  data[1] = HighB(CoDrone.droneImuRawAndAngle[0]);
+  data[2] = LowB(CoDrone.droneImuRawAndAngle[1]);   //AccY
+  data[3] = HighB(CoDrone.droneImuRawAndAngle[1]);
+  data[4] = LowB(CoDrone.droneImuRawAndAngle[2]);   //AccZ
+  data[5] = HighB(CoDrone.droneImuRawAndAngle[2]);
+  data[6] = LowB(CoDrone.droneImuRawAndAngle[3]);   //GyroRoll
+  data[7] = HighB(CoDrone.droneImuRawAndAngle[3]);
+  data[8] = LowB(CoDrone.droneImuRawAndAngle[4]);   //GyroPitch
+  data[9] = HighB(CoDrone.droneImuRawAndAngle[4]);
+  data[10] = LowB(CoDrone.droneImuRawAndAngle[5]);  //GyroYaw
+  data[11] = HighB(CoDrone.droneImuRawAndAngle[5]);
+  data[12] = LowB(CoDrone.droneImuRawAndAngle[6]);  //AngleRoll
+  data[13] = HighB(CoDrone.droneImuRawAndAngle[6]);
+  data[14] = LowB(CoDrone.droneImuRawAndAngle[7]);  //AnglePitch
+  data[15] = HighB(CoDrone.droneImuRawAndAngle[7]);
+  data[16] = LowB(CoDrone.droneImuRawAndAngle[8]);  //AngleYaw
+  data[17] = HighB(CoDrone.droneImuRawAndAngle[8]);
+  data[19] = CoDrone.Alive.ImuRawAndAngle;
+  Send_Processing(tType_ImuRawAndAngl, data, len);
+}
+void SendPcClass::Send_Pressure()
+{
+  byte data[17];
+  byte len = 17;
+
+  for (int i = 0; i < len - 1; i++) {
+    data[i] = CoDrone.dronePressure[i];
+  }
+  data[16] = CoDrone.Alive.Pressure;
+  Send_Processing(tType_Pressure, data, len);
+}
+
+void SendPcClass::Send_Temperature()
+{
+  byte data[9];
+  byte len = 9;
+
+  for (int i = 0; i < len - 1; i++) {
+    data[i] = CoDrone.droneTemperature[i];
+  }
+  data[8] = CoDrone.Alive.Temperature;
+  Send_Processing(tType_Temperature, data, len);
+}
+void SendPcClass::Send_Processing(byte _cType, byte *_data, byte _length)
 {
   byte _packet[30];
 
@@ -362,38 +357,133 @@ void Send_Processing(byte _cType, byte *_data, byte _length)
   _packet[3] = _length;
 
   //DATA
-  for (int i = 0; i < _length ; i++)
-  {
+  for (int i = 0; i < _length ; i++) {
     _packet[i + 4] = _data[i];
   }
   mySerial.write(_packet, _length + 4);
 }
 
 
-void eep_write()
+void IrSensClass::get_sens()
 {
-  byte* p = (byte*)&EEP;
-  for (int j = 0; j < sizeof(EEPSTRUCT); j++) {
-    EEPROM.write(j + EEP_START_ADRESS, *p);
-    p++;
+  byte ir[8];
+  for (int i = 0; i < 8; i++) {
+    ir[i] = digitalRead(11 + i);
+  }
+  Data = 0;
+  for (int i = 0; i < 8; i++) {
+    Data |= (ir[i] << (7 - i));
   }
 }
 
-void eep_read()
-{
-  byte* p = (byte*)&EEP;
-  for (int j = 0; j < sizeof(EEPSTRUCT); j++) {
-    *p = EEPROM.read(j + EEP_START_ADRESS);
-    p++;
+void IrSensClass::port_setting() {
+  for (int i = 0; i < 8; i++) {
+    pinMode(11 + i, INPUT);
   }
 }
 
-void clearAndHome()
+void ReceivePcClass::Receive()
 {
-  mySerial.write(27); // ESC
-  mySerial.print("[2J"); // clear screen
-  mySerial.write(27); // ESC
-  mySerial.print("[H"); // cursor to home
+  if ( mySerial.available() >= 0 ) {
+    int input = mySerial.read();
+    cmdBuff[cmdIndex++] = (byte)input;
+
+    if (cmdIndex >= MAX_PACKET_LENGTH) {
+      checkHeader = 0;
+      cmdIndex = 0;
+    } else {
+      if (cmdIndex == 1) {
+        if (cmdBuff[0] == START1) {
+          checkHeader = 1;
+        } else {
+          checkHeader = 0;
+          cmdIndex = 0;
+        }
+      } else if (cmdIndex == 2)
+      {
+        if (checkHeader == 1) {
+          if (cmdBuff[1] == START2) {
+            checkHeader = 2;
+          } else {
+            checkHeader = 0;
+            cmdIndex = 0;
+          }
+        }
+      } else if (checkHeader == 2) {
+        if (cmdIndex == 3) {
+          receiveDtype =  cmdBuff[2];
+          dataBuff[cmdIndex - 3] = cmdBuff[cmdIndex - 1];
+        } else if (cmdIndex == 4) {
+          receiveLength = cmdBuff[3];
+          dataBuff[cmdIndex - 3] = cmdBuff[cmdIndex - 1];
+        } else if (cmdIndex > 4) {
+          if (receiveLength + 4 >= cmdIndex) {
+            dataBuff[cmdIndex - 3] = cmdBuff[cmdIndex - 1];
+          }
+          if (receiveLength + 4 <= cmdIndex) {
+            if (receiveDtype == rType_Command) {
+              Receive_Command();
+            } else if (receiveDtype == rType_TrimAll) {
+              Receive_TrimAll();
+            } else if (receiveDtype == rType_Target) {
+              Receive_Target();
+            } else if (receiveDtype == rType_Output) {
+              Receive_Output();
+            } else if (receiveDtype == rType_Gains) {
+              Receive_Gains();
+            }
+            checkHeader = 0;
+            cmdIndex = 0;
+          }
+        }
+      } else {
+        checkHeader = 0;
+        cmdIndex = 0;
+      }
+    }
+  }
+}
+
+
+void ReceivePcClass::Receive_Command()
+{
+  Command.Command1 = dataBuff[2];
+  Command.Command2 = dataBuff[3];
+  Command.Command3 = dataBuff[4];
+  Command.Command4 = dataBuff[5];
+}
+
+void ReceivePcClass::Receive_TrimAll()
+{
+  EepMgr.Data.YawTrim = UniteByte(dataBuff[3], dataBuff[2]);
+  EepMgr.Data.ThrottleTrim = UniteByte(dataBuff[5], dataBuff[4]);
+  EepMgr.Data.PitchTrim = UniteByte(dataBuff[7], dataBuff[6]);
+  EepMgr.Data.RollTrim = UniteByte(dataBuff[9], dataBuff[8]);
+}
+
+void ReceivePcClass::Receive_Target()
+{
+  Target.Yaw =  UniteByte(dataBuff[3], dataBuff[2]);
+  Target.Throttle  = UniteByte(dataBuff[5], dataBuff[4]);
+  Target.Pitch = UniteByte(dataBuff[7], dataBuff[6]);
+  Target.Roll = UniteByte(dataBuff[9], dataBuff[8]);  
+}
+
+void ReceivePcClass::Receive_Gains()
+{
+
+}
+
+void ReceivePcClass::Receive_Output()
+{
+  Output.Yaw =  UniteByte(dataBuff[3], dataBuff[2]);
+  Output.Throttle  = UniteByte(dataBuff[5], dataBuff[4]);
+  Output.Pitch = UniteByte(dataBuff[7], dataBuff[6]);
+  Output.Roll = UniteByte(dataBuff[9], dataBuff[8]);
+}
+
+int ReceivePcClass::UniteByte(byte upper, byte lower) {
+  return ((upper << 8) | (lower & 0xFF) );
 }
 
 
